1d536df5be70fc02f1e1a163fb1b93414c4cc383
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-10-23 01:01:17 -0700

    random joke

---
+++ axios/app.js
@@ -0,0 +1,8 @@
+
+const getDadJoke = async () => {
+    const config= {headers: {Accept: 'application/json'}}
+    const res = await axios.get('https://icanhazdadjoke.com',config);
+    console.log(res.data.joke);
+}
+
+getDadJoke()
